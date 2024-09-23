63e998a1ed5d2041312bb7479d91dc8a07dbdbda
mohamedsellim <mohamed.selim@paysky.io>
2024-09-23 16:34:33 +0300

    add rejected mobile number test case

--- src/main/java/io/paysky/qa/utilities/testdata/Constant.java
+++ src/main/java/io/paysky/qa/utilities/testdata/Constant.java
@@ -2,6 +2,7 @@ package io.paysky.qa.utilities.testdata;
 
 public class Constant {
     public static final String EG_MOBILE_NUMBER = "01099240614";
+    public static final String REJECTED_MOBILE_NUMBER = "01000488466";
     public static final String PIN_NUMBER_1 = "1";
     public static final String PIN_NUMBER_2 = "4";
     public static final String PIN_NUMBER_3 = "7";
--- src/test/java/io/paysky/qa/yalla/LoginTest.java
+++ src/test/java/io/paysky/qa/yalla/LoginTest.java
@@ -1,10 +1,7 @@
 package io.paysky.qa.yalla;
-import io.paysky.qa.pages.PinCodePage;
+import io.paysky.qa.pages.*;
 import io.paysky.qa.utilities.sharedComponent.SharedComponent;
 import io.paysky.qa.utilities.testdata.Constant;
-import io.paysky.qa.pages.OnboardingPage;
-import io.paysky.qa.pages.LoginPage;
-import io.paysky.qa.pages.HomePage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -14,8 +11,9 @@ public class LoginTest {
     final LoginPage loginPage = new LoginPage();
     final PinCodePage pinCodePage = new PinCodePage();
     final HomePage homePage = new HomePage();
-    final SharedComponent sharedComponent = new SharedComponent();
-    String expectedText = "Yalla Merchant App";
+    final RejectedLoginPage rejectedLoginPage = new RejectedLoginPage();
+    String yallaMerchantExpectedText = "Yalla Merchant App";
+    String rejectedNumberExpectedText = "Account Rejected";
 
     public LoginTest() throws Exception {
     }
@@ -27,14 +25,17 @@ public class LoginTest {
         loginPage.clickOnNextButton();
         pinCodePage.enterPin(Constant.DEFAULT_PIN);  // Pass the PIN array dynamically
         pinCodePage.clickOnLoginButton();
-       // Assert.assertEquals(homePage.getMerchantYallaAppLogoText(), expectedText,
-         //       "Verify the Yalla Merchant App text is displayed correctly.");
-        Thread.sleep(5000);
-        homePage.clickOnBillsIcond();
+        Assert.assertEquals(homePage.getMerchantYallaAppLogoText(), yallaMerchantExpectedText,
+                "Verify the Yalla Merchant App text is displayed correctly.");
     }
 
     @Test(priority = 1)
-    public void validLoginWithMobileNumbers() throws Exception {
-        sharedComponent.loginToMainMenu();
+    public void LoginWithRejectedMobileNumber() throws Exception {
+        onboardingPage.clickOnSkipButton();
+        loginPage.enterMobileNumberOnLoginScreen(Constant.REJECTED_MOBILE_NUMBER);
+        loginPage.clickOnNextButton();
+        Assert.assertEquals(rejectedLoginPage.getMRejectedText(), rejectedNumberExpectedText,
+                "Verify the Yalla Merchant App text is displayed correctly.");
+
     }
 }
